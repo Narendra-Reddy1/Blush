84dc7e68fd75dcc306de29a8d04d5e9da7b1f816
Narendra-Reddy1 <58972366+Narendra-Reddy1@users.noreply.github.com>
2022-08-11 11:30:22 +0530

    Added MainMenu scene

--- Assets/_Game/_Scripts/InitSceneManager.cs
+++ Assets/_Game/_Scripts/InitSceneManager.cs
@@ -3,26 +3,35 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class InitSceneManager : MonoBehaviour
 {
-    [SerializeField] private string m_persistentScene;
-    [SerializeField] private string m_tutorial;
-    [SerializeField] private string m_persistentLevel;
+    //[SerializeField] private string m_persistentScene;
+    [SerializeField] private AssetReference m_tutorial;
+    [SerializeField] private AssetReference m_persistentLevel;
+    [SerializeField] private AssetReference m_persistentManagers;
+
+    private AsyncOperationHandle persistantSceneHandle;
+
 
     public void Awake()
     {
         LoadPersistenScene();
     }
-    public void LoadPersistenScene()
+    public async void LoadPersistenScene()
     {
-        SceneManager.LoadSceneAsync(m_persistentScene).completed += (handle) =>
-        {
-            SceneManager.LoadScene(m_tutorial, LoadSceneMode.Additive);
-            SceneManager.LoadSceneAsync(m_persistentLevel, LoadSceneMode.Additive);
-            //SceneManager.SetActiveScene()
+        /*  SceneManager.LoadSceneAsync(m_persistentScene).completed += (handle) =>
+          {
+              SceneManager.LoadScene(m_tutorial, LoadSceneMode.Additive);
+              SceneManager.LoadSceneAsync(m_persistentLevel, LoadSceneMode.Additive);
+              //SceneManager.SetActiveScene()
 
 
-        };
+          };*/
+        persistantSceneHandle = m_persistentManagers.LoadSceneAsync(LoadSceneMode.Single, true);
+        await persistantSceneHandle.Task;
+        m_persistentLevel.LoadSceneAsync(LoadSceneMode.Additive, true);
     }
 }
